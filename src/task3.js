697ca723592e9b4874267b6ab2007106a152c0a8
Tetiana <petrovska.tetiana@lll.kpi.ua>
2026-03-31 16:35:48 +0000

    Enhance memoization function with options for cache strategy and size; remove test.js file

--- src/task3.js
+++ src/task3.js
@@ -1,42 +1,83 @@
-function memoized(fnIN) {
+function memoized(fnIN, options) {
 
-    const cache = new Map();
+  const cache = new Map();
 
-    return function (...args) { //повертаємо нову функцію при виклику memoized(sum);
+  return function (...args) {
+    //повертаємо нову функцію при виклику memoized(sum);
+    const key = JSON.stringify(args); //створюємо ключ на основі аргументів
 
-        const key = JSON.stringify(args); //створюємо ключ на основі аргументів
+    if (cache.has(key)) {
+      // якщо є кешоване значення для цього ключа
+      const cacheEntry = cache.get(key); // отримуємо кешоване значення
 
-        if (cache.has(key)) { // якщо є кешоване значення для цього ключа
+      if (options.ttl && Date.now() - cacheEntry.createdAt > options.ttl) {
+        cache.delete(key); //видаляє запис, якщо він перебуває в кеші довше, ніж дозволено. (Time-Based Expiry)
+      } else {
 
-            const cacheEntry = cache.get(key); // отримуємо кешоване значення
-            cacheEntry.lastAccessed = Date.now();
-            cacheEntry.accessCount += 1;
-            return cacheEntry.value;
-
+        if (options.strategy === "LRU") {
+          cache.delete(key); 
+          cache.set(key, cacheEntry);
         }
 
-        const result = fnIN(...args);
+        cacheEntry.lastAccessed = Date.now();
+        cacheEntry.accessCount += 1;
+        return cacheEntry.value;
+      }
+    }
 
-        cache.set(key, { // зберігаємо результат у кеші
+    if (options.maxSize && cache.size >= options.maxSize){
 
-            value: result,
-            createdAt: Date.now(),
-            lastAccessed: Date.now(),
-            accessCount: 1
+      if (options.strategy === 'LRU') {
+        const oldestKey = cache.keys().next().value;
+        cache.delete(oldestKey);
+      }
+      else if (options.strategy === 'LFU') {
+        let minKey = null;
+        let minCount = Infinity;
+        for (const [kay, value] of cache) {
+          if (value.accessCount < minCount) {
+            minCount = value.accessCount;
+            minKey = kay;
+          }
+        }
+        cache.delete(minKey);
 
-        });
-
-        return result;
+      } else if (typeof options.strategy === "function") {
+        const keyToDelete = options.strategy(cache);
+        if (keyToDelete) cache.delete(keyToDelete);
+      }
 
     }
+
+    const result = fnIN(...args);
+
+    cache.set(key, {
+      // зберігаємо результат у кеші
+      value: result,
+      createdAt: Date.now(),
+      lastAccessed: Date.now(),
+      accessCount: 1,
+    });
+
+    return result;
+  };
 }
 
+function options(strategyType, size, timeToLive) {
+  return {
+    strategy: strategyType,
+    maxSize: size,
+    ttl: timeToLive,
+  };
+}
+
+const myOptions = options("LRU", 3, 5000); // обираємо політику LRU, LFU, TTL
 
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
 
-const memoizedSum = memoized(sum);
+const memoizedSum = memoized(sum, myOptions);
 memoizedSum(1, 2);
 
 /*cache.has(key)      // чи є такий ключ
